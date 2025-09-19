1d29e16b2533d3eb22d10fd1fb3510206fe9185e
Unorishi <rishikeshamathi34@gmail.com>
2025-09-19 16:46:49 +0530

    feat:Add login module

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -11,5 +11,15 @@ router.route('/signup').post(async (req, res) => {
         res.status(400).json({ error: err.message });
     }
 });
+router.route('/login').post(async (req, res) => {
+    const { username, password } = req.body;
+    const newUser = new User({ username, password });
+    try {
+        await newUser.save();
+        res.status(201).json({ message: 'User created successfully' });
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+});
 
 module.exports = router;
