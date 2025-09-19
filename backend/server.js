17c6075e2f8b6e8f13e965a72bb6c2b1f55b0433
Unorishi <rishikeshamathi34@gmail.com>
2025-09-19 11:32:14 +0530

    Initial project structure with basic frontend and backend

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const  mongoose = require('mongoose');
+const cors = require('cors');
+
+require('dotenv').config();
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = 'mongodb://localhost:27017/mydatabase';
+mongoose.connect(uri).then(() => {
+    console.log('Connected to MongoDB');
+}).catch(err => {
+    console.error('Error connecting to MongoDB:', err);
+});
+
+app.get('/', (req, res) => {
+    res.send('Backend is running');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
